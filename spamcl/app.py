7f727396bc5a4c8bc6f948c055da73910f6b8ff5
Soro Kolotioloma <43370336+sorokolo@users.noreply.github.com>
2020-12-04 21:53:31 +0200

    Update app.py

--- spamcl/app.py
+++ spamcl/app.py
@@ -22,6 +22,8 @@ from nltk.corpus import stopwords
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model 
+from flask import Flask, request, render_template
+
 
 
 model = load_model("model/network.h5") 
@@ -29,57 +31,72 @@ model = load_model("model/network.h5")
 with open('model/tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-app = flask.Flask(__name__, template_folder='templates')
-@app.route('/', methods=['GET', 'POST'])
-def main():
+label2int = {'0': 0, '1': 1}
+int2label = {0: '0', 1: '1'}
+SEQUENCE_LENGTH = 100 
 
-    label2int = {'0': 0, '1': 1}
-    int2label = {0: '0', 1: '1'}
-    SEQUENCE_LENGTH = 100 
+def preprocess_text(sen):
+    sentence = remove_tags(sen)
+    sentence = re.sub('[^a-zA-Z]', ' ', str(sentence))
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', str(sentence))
+    sentence = re.sub(r'\s+', ' ', str(sentence))
 
-    def preprocess_text(sen):
-        sentence = remove_tags(sen)
-        sentence = re.sub('[^a-zA-Z]', ' ', str(sentence))
-        sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', str(sentence))
-        sentence = re.sub(r'\s+', ' ', str(sentence))
+    return sentence
 
-        return sentence
+TAG_RE = re.compile(r'<[^>]+>')
+def remove_tags(text):
+    return TAG_RE.sub('', str(text))
+
+def get_predictions(text):
+    text = preprocess_text(text)
+    sequence = tokenizer.texts_to_sequences([text])
+    # pad the sequence
+    sequence = pad_sequences(sequence, maxlen=SEQUENCE_LENGTH)
+    # get the prediction
+    prediction = model.predict(sequence)[0]
+    # one-hot encoded vector, revert using np.argmax
+    return int2label[np.argmax(prediction)]
+
+
+def show_predict(email_text):
+    prediction = get_predictions(email_text)
+    email_type = ""
+    if(prediction == '0'):
+        email_type = "The prediction is "+str(prediction)+" It is a Spam"
+    else:
+        email_type = "The prediction is "+str(prediction)+" It is not a Spam"
     
-    TAG_RE = re.compile(r'<[^>]+>')
-    def remove_tags(text):
-        return TAG_RE.sub('', str(text))
-
-    def get_predictions(text):
-        text = preprocess_text(text)
-        sequence = tokenizer.texts_to_sequences([text])
-        # pad the sequence
-        sequence = pad_sequences(sequence, maxlen=SEQUENCE_LENGTH)
-        # get the prediction
-        prediction = model.predict(sequence)[0]
-        # one-hot encoded vector, revert using np.argmax
-        return int2label[np.argmax(prediction)]
+    return email_type
 
 
-    if flask.request.method == 'GET':
-        return(flask.render_template('main.html'))
 
-    if flask.request.method == 'POST':
-        email_text = flask.request.form['email_text']
 
-        prediction = get_predictions(email_text)
-        email_type = ""
-        if(prediction == '0'):
-            email_type = "The prediction is "+str(prediction)+" It is a Spam"
-        else:
-            email_type = "The prediction is "+str(prediction)+" It is not a Spam"
+app = Flask(__name__)
 
-        return flask.render_template('main.html',
-        original_input={
-            "email_text":email_text,
-                                                        
-        },
-                                        result=email_type,
-                                        )
+@app.route('/')
+def home():
+    return render_template('main.html')
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+  
+
+    data = request.form.get('email_text')
+    prediction = ""
+    if data == None:
+        prediction = 'Got None'
+    else:
+        # model.predict.predict returns a dictionarys
+        prediction = show_predict(data)
+    # return json.dumps(str(prediction))
+
+    return render_template('main.html',  original_input={
+            "email_text":data,}, 
+        
+        prediction=prediction)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
